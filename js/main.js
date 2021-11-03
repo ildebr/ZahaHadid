e55eb33d16caa3e9e42c3535f50208752b0d36ce
ild <ildebrandoefrain@gmail.com>
2021-11-03 18:58:38 -0430

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+// const setHeight = () =>{
+// 	const nav = parseInt($("nav.navbar").css("height"));
+// 	const header = $("header");
+
+// 	$(header).css("height", function(self){
+// 		console.log($(this).height())
+// 		console.log(self)
+// 		return $(this).height() - nav;
+// 	})
+// }
+
+// $(document).ready(function(){
+//     console.log("perra3");
+//     setHeight();
+// })
