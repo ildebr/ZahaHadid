7e2527a49e60532d39a331c11970a248e0fd670f
ild <ildebrandoefrain@gmail.com>
2021-11-06 09:27:40 -0430

    animacion de letras añadida

--- js/main.js
+++ js/main.js
@@ -1,16 +1,9 @@
 
-// const setHeight = () =>{
-// 	const nav = parseInt($("nav.navbar").css("height"));
-// 	const header = $("header");
-
-// 	$(header).css("height", function(self){
-// 		console.log($(this).height())
-// 		console.log(self)
-// 		return $(this).height() - nav;
-// 	})
-// }
-
-// $(document).ready(function(){
-//     console.log("perra3");
-//     setHeight();
-// })
+var typed3 = new Typed('.main-title', {
+    strings: ['Architect', 'Artist', 'Designer'],
+    typeSpeed: 40,
+    backSpeed: 40,
+    smartBackspace: false, // this is a default
+    loop: true,
+    cursorChar: '',
+  });
