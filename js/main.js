a7d3d4fec0ff84792ac9f2698210bcb04addd6d8
ild <ildebrandoefrain@gmail.com>
2021-11-06 21:00:47 -0430

    new section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 var typed3 = new Typed('.main-title', {
-    strings: ['Architect', 'Artist', 'Designer'],
+    strings: ['Architect', 'Artist', 'Designer', 'Zaha Hadid'],
     typeSpeed: 40,
     backSpeed: 40,
     smartBackspace: false, // this is a default
